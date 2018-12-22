16054491cac2063d0fae663847eadc726557408b
Kerfume <kerfume@icloud.com>
2018-12-22 13:06:34 +0900

    add java packages

---
+++ scala/ensime/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/scala/src
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.144-0.b01.el7_4.x86_64" >> ~/.zshrc
+# echo 'alias java="docker exec -it ensime java"' >> ~/.zshrc
